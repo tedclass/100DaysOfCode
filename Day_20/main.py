0c1c0c64c6538920faa197b1be5c7a1a0be6569f
Ted <kimanited@ymail.com>
2024-04-30 10:35:04 +0300

    Just a few updates

---
+++ Day_20/main.py
@@ -0,0 +1,12 @@
+from turtle import Screen, Turtle
+
+#Creating a screen instance
+screen = Screen()
+screen.setup(width=600, height= 600)
+screen.bgcolor('black')
+screen.title()('My Snake Game')
+
+
+
+
+screen.exitonclick()
